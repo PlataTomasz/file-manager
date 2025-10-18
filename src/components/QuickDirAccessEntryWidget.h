b3c75e816e40ae7b1e300b1b8289d296a96f857c
Tomasz <tomaszplata10@gmail.com>
2025-10-18 15:13:59 +0200

    Documented classes related to quick directory access.

--- src/components/QuickDirAccessEntryWidget.h
+++ src/components/QuickDirAccessEntryWidget.h
@@ -8,10 +8,16 @@
 #include <qwidget.h>
 #include <QLabel>
 
+/**
+Represents entry on the quick access widget. Containing an absolute path.
+*/
 class QuickDirAccessEntryWidget : public QWidget
 {
     Q_OBJECT
 private:
+    /**
+    Stores current path, which is displayed on widget and where it should lead after double clicking.
+    */
     QString path;
     QLabel *label = nullptr;
     QGridLayout *layout = nullptr;
@@ -19,12 +25,21 @@ private:
 protected:
     void mouseDoubleClickEvent(QMouseEvent *event) override;
 signals:
+    /**
+    Informs that this entry was double clicked.
+    */
     void doubleClicked();
 public:
     explicit QuickDirAccessEntryWidget(QWidget *parent = nullptr);
     explicit QuickDirAccessEntryWidget(QString path, QWidget *parent = nullptr);
 
+    /**
+    @param path New path to be set.
+    */
     void setPath(QString path);
+    /**
+    @return path
+    */    
     QString getPath();
 };
 
--- src/components/QuickDirectoryAccessWidget.h
+++ src/components/QuickDirectoryAccessWidget.h
@@ -9,6 +9,9 @@
 #include <qwidget.h>
 #include <ui_QuickDirectoryAccessWidget.h>
 
+/**
+Widget, which displays quick access directories. Visually It's a list of common directories. Each can be clicked to move instantly to that directory. 
+*/
 class QuickDirectoryAccessWidget : public QWidget
 {
     Q_OBJECT
@@ -16,11 +19,23 @@ private:
     Ui::QuickDirectoryAccessWidget ui;
     std::list<QuickDirAccessEntryWidget *> entries;
 signals:
+    /**
+    Informs that one of the directories from widget wants to be opened.
+    @param string Absolute path of directory that wants to be opened
+    */
     void directoryOpened(QString string);
 public:
     explicit QuickDirectoryAccessWidget(QWidget *parent = nullptr);
 
+    /**
+    Adds new entry to the list of directories.
+    @param path Absolute path to entry.
+    */
     void addEntry(QString path);
+    /**
+    Removes first entry with matching path from the list of directories.
+    @param path Path to directory, that needs to be removed from widget. Can be either relative or absolute.
+    */
     void removeEntry(QString path);
 };
 
