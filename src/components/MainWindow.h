675408b845916dd828994f6ede128961f549f13a
Tomasz <tomaszplata10@gmail.com>
2025-10-18 15:26:27 +0200

    Documented main window

--- src/components/MainWindow.h
+++ src/components/MainWindow.h
@@ -9,11 +9,17 @@
 #include "DirectoryNavigationService.h"
 #include "ui_MainWindow.h"
 
-
+/**
+Represents main window of the file manager. It manages communication between DirectoryNavigationService and other objects, so they can alter current directory or receive notifications that indicate the current directory changed.
+*/
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 public:
+    /**
+    @param directoryNavigationService DirectoryNavigationService instance to be used by main window.
+    @param parent Parent of this widget.
+    */
     explicit MainWindow(
         DirectoryNavigationService *directoryNavigationService,
         QWidget *parent = nullptr
