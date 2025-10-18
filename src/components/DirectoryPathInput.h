f3b617646a9571679ef7ee6427a69f804591b5ce
Tomasz <tomaszplata10@gmail.com>
2025-10-18 15:31:26 +0200

    Documented address bar related class

--- src/components/DirectoryPathInput.h
+++ src/components/DirectoryPathInput.h
@@ -6,18 +6,33 @@
 #include <qtmetamacros.h>
 #include <qwidget.h>
 
+/**
+Represents address bar. Allows for manual input of target path or copying of the current one. 
+*/
 class DirectoryPathInput : public QLineEdit
 {
     Q_OBJECT
 public:
     explicit DirectoryPathInput(QWidget *parent = nullptr);
-
+    /**
+    @returns Current directory absolute path, which is visible in this widget.
+    */
     QString getCurrentDirectoryPath();
+    /**
+    Changes current directory path visible in this widget.
+    @param currentDirPath 
+    */
     void setCurrentDirectoryPath(QString currentDirPath);
 signals:
-    // User prompted a valid directory path
+    /**
+    Informs that user prompted a valid directory path.
+    @param path Path that used prompted
+    */
     void validDirectoryPathPrompted(QString path);
-    // User prompted an invalid directory path - i.e. non-existant one
+    /**
+    Informs that user prompted an invalid directory path - i.e. non-existant one
+    @param path Path that used prompted
+    */
     void invalidDirectoryPathPrompted(QString path);
 protected:
     
